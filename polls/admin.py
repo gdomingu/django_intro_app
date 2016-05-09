ee30c8ba40f638383374494d3c26d96830b82f8c
Gabe D <gpad88@gmail.com>
2016-05-09 13:21:27 -0700

    easily customized admin and got my mind blown

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+#wow that admin.StackedInline/TabularInline is AWESOME!
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
+
+# for custimization:
+class QuestionAdmin(admin.ModelAdmin):
+  #setting these variables sets the options for class
+  fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 
+          'classes': ['collapse']}),
+    ]
+  inlines = [ChoiceInline]
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+  list_filter = ['pub_date']
+  search_fields = ['question_text']
+# pass in the model admin class as a second argument 
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
